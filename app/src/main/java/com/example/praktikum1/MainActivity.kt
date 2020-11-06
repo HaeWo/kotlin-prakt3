22f0cf22e3d89dd3b9470dd02450c0bfe758512a
mehyar <mehyar.al-shammas@hs-bochum.de>
2020-11-06 14:35:52 +0100

    Using a JSON variable to save data instead of Kotlin class/object and change the saving method to run automatically every second 🔄

--- app/src/main/java/com/example/praktikum1/MainActivity.kt
+++ app/src/main/java/com/example/praktikum1/MainActivity.kt
@@ -12,12 +12,14 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.delay
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
@@ -37,12 +39,6 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     //var jsonObjectAsString = jsonObject.toString()
     //var jsonArrayAsString = jsonArray.toString()
 
-
-    //Sensor variables
-    private var gravityData: SensorData? = null
-    private var accelerometerData: SensorData? = null
-    private var gyroscopeData: SensorData? = null
-
     //TextViews
     private lateinit var acc: TextView
     private lateinit var grv: TextView
@@ -55,7 +51,7 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     private lateinit var btnStart: Button
 
     //Time variables
-    private var dt:Long = 10006
+    private var dt:Long = 1000
     private var counter:Long = 0
 
     private var start = false
@@ -73,13 +69,14 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         //The start/stop button
         btnStart.setOnClickListener{
             if(!start){
-                getLocation()
                 start = true
+                getLocation()
                 registerListener()
                 btnStart.text = "Stop"
             }else{
                 start = false
                 unregisterListener()
+                saveFile()
                 btnStart.text = "Start"
             }
         }
@@ -120,11 +117,11 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     }
 
     private fun registerListener(){
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST)
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_FASTEST)
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_FASTEST)
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_FASTEST)
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     private fun unregisterListener(){
@@ -134,111 +131,84 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     @SuppressLint("MissingPermission")
     private fun getLocation(){
         locationListener = LocationListener { location ->
-            if(start){
-                loc.text = "Latitude: ${location.latitude}\nLongitude: ${location.longitude}"
-            }
+            loc.text = "Latitude: ${location.latitude}\nLongitude: ${location.longitude}"
         }
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1L, 1f, locationListener)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        //if statement to update the values every 100ms
+
         if(System.currentTimeMillis() - counter >= dt) {
-            /*update the values of the sensors when they change
-            and writing them into an object.
-            The if statement ensure to create an object at first
-            then update the values of the already created object and NOT
-            creating new objects*/
             when (event?.sensor?.type) {
-                Sensor.TYPE_ACCELEROMETER -> if (accelerometerData == null) {
-                    accelerometerData = SensorData(
-                        event.values[0],
-                        event.values[1],
-                        event.values[2],
-                        event.timestamp
-                    )
-                } else {
-                    accelerometerData!!.x = event.values[0]
-                    accelerometerData!!.y = event.values[1]
-                    accelerometerData!!.z = event.values[2]
-                    accelerometerData!!.timestap = event.timestamp
+                Sensor.TYPE_ACCELEROMETER -> {
+                jsonObject.put("X",event.values[0])
+                jsonObject.put("Y",event.values[1])
+                jsonObject.put("Z",event.values[2])
                 }
             }
+            jsonArray.put(jsonObject)
+
             when (event?.sensor?.type) {
-                Sensor.TYPE_GYROSCOPE -> if (gyroscopeData == null) {
-                    gyroscopeData = SensorData(
-                        event.values[0],
-                        event.values[1],
-                        event.values[2],
-                        event.timestamp
-                    )
-                } else {
-                    gyroscopeData!!.x = event.values[0]
-                    gyroscopeData!!.y = event.values[1]
-                    gyroscopeData!!.z = event.values[2]
-                    gyroscopeData!!.timestap = event.timestamp
+                Sensor.TYPE_GYROSCOPE ->{
+                    jsonObject.put("X",event.values[0])
+                    jsonObject.put("Y",event.values[1])
+                    jsonObject.put("Z",event.values[2])
                 }
             }
+            jsonArray.put(jsonObject)
+
             when (event?.sensor?.type) {
-                Sensor.TYPE_GRAVITY -> if (gravityData == null) {
-                    gravityData = SensorData(
-                        event.values[0],
-                        event.values[1],
-                        event.values[2],
-                        event.timestamp
-                    )
-                } else {
-                    gravityData!!.x = event.values[0]
-                    gravityData!!.y = event.values[1]
-                    gravityData!!.z = event.values[2]
-                    gravityData!!.timestap = event.timestamp
+                Sensor.TYPE_GRAVITY -> {
+                    jsonObject.put("X",event.values[0])
+                    jsonObject.put("Y",event.values[1])
+                    jsonObject.put("Z",event.values[2])
                 }
             }
-            //reset the value to count a new 100ms
+            jsonArray.put(jsonObject)
+
             counter = System.currentTimeMillis()
-        }
+        }else{
+            when (event?.sensor?.type) {
+                Sensor.TYPE_ACCELEROMETER -> acc.text =
+                    "X: ${"%.2f".format(event.values[0])} m/s² \n" +
+                            " Y: ${"%.2f".format(event.values[1])} m/s² \n" +
+                            " Z: ${"%.2f".format(event.values[2])} m/s² "
+            }
 
-        //read the values from the created object and show it on screen
-        getData()
+            when (event?.sensor?.type) {
+                Sensor.TYPE_GYROSCOPE -> gyr.text =
+                    "X: ${"%.2f".format(event.values[0])} m/s² \n" +
+                            " Y: ${"%.2f".format(event.values[1])} m/s² \n" +
+                            " Z: ${"%.2f".format(event.values[2])} m/s² "
+            }
 
-        /*the pressure and light sensors will not be written into an object
-        and will not be saved into file later, they will only be shown at the screen*/
-        when (event?.sensor?.type) {
-            Sensor.TYPE_PRESSURE -> pre.text = "${"%.2f".format(event.values[0])} hPa"
-        }
-        when (event?.sensor?.type) {
+            when (event?.sensor?.type) {
+                Sensor.TYPE_GRAVITY -> grv.text =
+                    "X: ${"%.2f".format(event.values[0])} m/s² \n" +
+                            " Y: ${"%.2f".format(event.values[1])} m/s² \n" +
+                            " Z: ${"%.2f".format(event.values[2])} m/s² "
+            }
+
+            when (event?.sensor?.type) {
+                Sensor.TYPE_PRESSURE -> pre.text = "${"%.2f".format(event.values[0])} hPa"
+            }
+
+            when (event?.sensor?.type) {
                 Sensor.TYPE_LIGHT -> lit.text = "${"%.2f".format(event.values[0])} lx"
             }
+        }
+
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 
-    private fun getData(){
-        if(gravityData != null) {
-                grv.text = "X: ${"%.2f".format(gravityData!!.x)} m/s² \n" +
-                        " Y: ${"%.2f".format(gravityData!!.y)} m/s² \n" +
-                        " Z: ${"%.2f".format(gravityData!!.z)} m/s² "
-        }
-        if(accelerometerData != null){
-            acc.text = " X: ${"%.2f".format(accelerometerData!!.x)} m/s²" +
-                    "\n Y: ${"%.2f".format(accelerometerData!!.y)} m/s² \n" +
-                    " Z: ${"%.2f".format(accelerometerData!!.z)} m/s² "
-        }
-        if(gyroscopeData != null){
-            gyr.text = "X: ${"%.2f".format(gyroscopeData!!.x)} m/s² \n" +
-                    " Y: ${"%.2f".format(gyroscopeData!!.y)} m/s² \n" +
-                    " Z: ${"%.2f".format(gyroscopeData!!.z)} m/s² "
-        }
-    }
-
     private fun saveFile(){
 
-        val fileData = "Gravity: "+gravityData.toString()+"\nAcceleration: "+accelerometerData.toString()+"\nGyroscope: "+gyroscopeData.toString()
-        var myExternalFile = File(getExternalFilesDir(""), "Output.txt")
+        var myExternalFile = File(getExternalFilesDir(""), "Output.json")
 
         try {
             val fileOutPutStream = FileOutputStream(myExternalFile)
-            fileOutPutStream.write(fileData.toByteArray())
+            fileOutPutStream.write(jsonArray.toString().toByteArray())
             fileOutPutStream.close()
         } catch (e: IOException) {
             e.printStackTrace()
--- app/src/main/java/com/example/praktikum1/SensorData.kt
+++
@@ -1,4 +0,0 @@
-package com.example.praktikum1
-
-data class SensorData (var x:Float, var y:Float, var z:Float, var timestap: Long){
-}
